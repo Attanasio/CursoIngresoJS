09101c1271da087cd9e55e4032167a91925a8fc0
alumno <alumno>
2019-02-08 12:34:19 -0300

    Ejercicios completos del 1 al 9

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primernumero;
+	var segundonumero;
+	var total;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	total=(primernumero+segundonumero);
+
+	alert("El total de la suma es "+total);
 }
 
 function restar()
 {
-	
+	var primernumero;
+	var segundonumero;
+	var total;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	total=(primernumero-segundonumero);
+
+	alert("El total de la resta es "+total);
 }
 
 function multiplicar()
 { 
-	
+	var primernumero;
+	var segundonumero;
+	var total;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	total=(primernumero*segundonumero);
+
+	alert("El total de la multiplicacion es "+total);
 }
 
 function dividir()
 {
-	
+	var primernumero;
+	var segundonumero;
+	var total;
+
+	primernumero=numeroUno.value;
+	segundonumero=numeroDos.value;
+
+	primernumero=parseInt(primernumero);
+	segundonumero=parseInt(segundonumero);
+
+	total=(primernumero/segundonumero);
+
+	alert("El total de la dividicion es "+total);
 }
 
