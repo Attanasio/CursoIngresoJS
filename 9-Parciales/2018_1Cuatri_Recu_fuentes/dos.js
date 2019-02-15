b54e451f2f0b56e732e6c2f86904dbb2e785cd08
alumno <alumno>
2019-02-15 10:18:33 -0300

    ejercicios

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
-  
+  	var dinero;
+  	var descuento;
+  	var iva;
+  	var masiva;
+
+  	dinero=elNombre.value;
+  	descuento=(dinero*0.90);
+  	iva=(dinero*21/100);
+
+  	descuento=parseInt(descuento);
+  	iva=parseInt(iva);
+
+  	masiva=(descuento+iva);
+
+  	alert("Tu compra es de $"+dinero+", tenes un descuento del 10%, queda en "+descuento+" mas el IVA es $"+masiva);
+
 }
